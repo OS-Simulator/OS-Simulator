ba8edbf8849e4cebb310394124c5c6f77a30fcb8
arvindsaik <krishnanarvind100@gmail.com>
2017-04-13 23:01:12 +0530

    Major UI change in Process scheduling bootstrap panels and buttons

--- ossim/ossim/static/js/input.js
+++ ossim/ossim/static/js/input.js
@@ -32,25 +32,39 @@ var index = 1;
   function displayList() {
       var output = document.getElementById("output");
       //Clear the op
-      output.innerHTML = '<div class="heading"><span> PNo.</span><span class="pad"> </span> <span>AT</span><span class="pad"> </span>  <span>BT</span>  </div>';
+      output.setAttribute("style", "height:"+stuff.length*50+"px");
+      output.innerHTML="";
+      //output.innerHTML = '<div class="heading"><span> PNo.</span><span class="pad"> </span> <span>AT</span><span class="pad"> </span>  <span>BT</span>  </div>';
       for (var i = 0; i < stuff.length; i++) {
           var card = document.createElement("div");
           var pno = document.createElement("span");
           pno.textContent = "P"+(i+1)+"    ";
+	  pno.setAttribute("style","float:left;");
+	  card.setAttribute("style","height:50px;width:100%;");
           card.setAttribute("class", "card");
+	  
           var input1 = document.createElement("input");
           input1.value = stuff[i].at;
+	  input1.setAttribute("class", "form-control");
+	  input1.setAttribute("style", "width:100px;float:left;margin-left:10px");
           input1.setAttribute("disabled", "disabled");
           input1.setAttribute("id", "arrivaltime" + i);
           var input2 = document.createElement("input");
           input2.value = stuff[i].bt;
+	  input2.setAttribute("class", "form-control");
+	  input2.setAttribute("style", "width:100px;");
           input2.setAttribute("disabled", "disabled");
           input2.setAttribute("id", "bursttime" + i);
+	  input2.setAttribute("style","float:left;width:100px;margin-left:10px;");
           var btn = document.createElement("button");
           var textNode = document.createTextNode("EDIT");
           btn.appendChild(textNode);
           btn.setAttribute("id", "btn" + i);
+	  btn.setAttribute("class", "btn btn-warning");
           btn.setAttribute("onclick", "edit(this.id)");
+	  btn.setAttribute("style","float:left;margin-left:10px;");
+	  var br = document.createElement("br");
+	  card.appendChild(pno);
           card.appendChild(pno);
           card.appendChild(input1);
           card.appendChild(input2);
@@ -59,7 +73,7 @@ var index = 1;
       }
   }
 
-  function download() {
+  /*function download() {
       var text = JSON.stringify(stuff);
       filearrivaltime = "stuff.json";
       var element = document.createElement('a');
@@ -70,4 +84,4 @@ var index = 1;
       element.click();
       document.body.removeChild(element);
 
-  }
+  }*/
