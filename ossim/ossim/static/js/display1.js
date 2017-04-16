0dd8fc2086b2d1c5417d36fb5e325aa5474a25eb
arvindsaik <krishnanarvind100@gmail.com>
2017-04-16 15:11:23 +0530

    Hopefully final

--- ossim/ossim/static/js/display1.js
+++ ossim/ossim/static/js/display1.js
@@ -5,10 +5,6 @@ var totalTime;
 var pixel;
 var i;
 var containerWidth;
-var avgwt = 0;
-var avgtat = 0;
-var type;
-var gtype;
 function toColor(num) {
     num >>>= 0;
     var b = num & 0xFF,
@@ -19,30 +15,24 @@ function toColor(num) {
 }
 
 function drawTable(i){
-    if(i<a.length){
+    if(a[i] == undefined) return;
     var table = document.getElementById("ptable");
-    console.log(a[i]);
-    if(a[i].pri==1) type = "FG";
-    else type = "BG";
-    $("#ptable").append("<tr><td>"+type+a[i].no+"</td><td>"+a[i].at+"</td><td>"+a[i].bt+"</td><td>"+a[i].ct+"</td><td>"+a[i].wt+"</td><td>"+a[i].tat+"</td></tr>");
-  }
-
+    console.log(a[i].pri);
+    $("#ptable").append("<tr><td>"+(i+1)+"</td><td>"+a[i].pri+"</td><td>"+a[i].at+"</td><td>"+a[i].bt+"</td><td>"+a[i].ct+"</td><td>"+a[i].wt+"</td><td>"+a[i].tat+"</td></tr>");
 }
 
 function displayBlock(i){
   if(i == total){
-    $('#gantth').append('<h2>AVG WT: '+(avgwt/a.length).toFixed(2)+' AVG TAT: '+(avgtat/a.length).toFixed(2)+'</h2>');
     //drawTable();
     return;
   }
 
-  if(sequence[i].no == -1) $("#comments").append("<br>t = "+sequence[i].start+": CPU is idle<br>");
-  else $("#comments").append("<br>t = "+sequence[i].start+" : "+ sequence[i].type +sequence[i].no+" entered CPU"+" and is being executed<br>");
-
+  if(sequence[i].no == -1) $("#comments").append("<br>t = "+sequence[i].start+"CPU is idle<br>");
+  else $("#comments").append("<br>t = "+sequence[i].start+" : Process "+sequence[i].no+" entered CPU"+" and is being executed<br>");
   var blockWidth = (sequence[i].stop - sequence[i].start)*pixel;
   var processName = sequence[i].no;
   if(sequence[i].no == -1) $('#outer-div').append('<div class="block" id="process-'+sequence[i].start +'">CPU IDLE' +'<div class="bottom">'+ sequence[i].stop +'</div></div>');
-  $('#outer-div').append('<div class="block" id="process-'+sequence[i].start +'">'+ sequence[i].type+sequence[i].no +'<div class="bottom">'+ sequence[i].stop +'</div></div>');
+  $('#outer-div').append('<div class="block" id="process-'+sequence[i].start +'">P-'+ sequence[i].no +'<div class="bottom">'+ sequence[i].stop +'</div></div>');
   $('#process-'+sequence[i].start).css('width',blockWidth);
   if(sequence[i].no != -1)
   $('#process-'+sequence[i].start).css('background-color',toColor(sequence[i].no - 500000*sequence[i].no*sequence[i].no*sequence[i].no*sequence[i].no));
